f88eb2584d7fdc298d87b6004ec84b878e780d17
thiennhanng191 <ntnguyen@vassar.edu>
2021-12-11 00:37:57 -0500

    implement infinite scroll

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -28,9 +28,9 @@ class HomeTableViewController: UITableViewController {
     }
 
     @objc func loadTweets() {
-        
+        numberOfTweets = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 20]
+        let myParams = ["count": numberOfTweets]
         
             
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets:  [NSDictionary]) in
@@ -49,6 +49,35 @@ class HomeTableViewController: UITableViewController {
         }, failure: { (Error) in
             print("Could not retrive tweets!")
         })
+    }
+    
+    func loadMoreTweets() {
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        
+        numberOfTweets += 20
+        
+        let myParams = ["count": numberOfTweets]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets:  [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+                        
+        }, failure: { (Error) in
+            print("Could not retrive tweets!")
+        })
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
     }
     
     @IBAction func onLogout(_ sender: Any) {
